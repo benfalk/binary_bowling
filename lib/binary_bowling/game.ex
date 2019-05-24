a59eeae0c10c843b54564987c2e0b65fcb5b24b6
Benjamin Falk <benjamin.falk@yahoo.com>
2019-05-24 13:24:54 -0400

    More work on the bowling game

--- lib/binary_bowling/game.ex
+++ lib/binary_bowling/game.ex
@@ -30,12 +30,22 @@ defmodule BinaryBowling.Game do
   alias __MODULE__, as: Game
 
   @type t :: binary()
+  @type error_msg :: :game_over
 
-  @blank_board <<0::16,  # Score
-                 0::2,   # Current Roll
-                 0::6,   # Current Frame
-                 0::72,  # First 9 Frames
-                 0::16>> # Last Frame
+  @blank_board <<
+    # Game Over Flag
+    0::1,
+    # Score
+    0::15,
+    # Current Roll
+    0::2,
+    # Current Frame
+    0::6,
+    # First 9 Frames
+    0::72,
+    # Last Frame
+    0::16
+  >>
 
   @doc """
   With an optional playername, returns a new bowling game
@@ -46,14 +56,109 @@ defmodule BinaryBowling.Game do
   end
 
   @spec score(Game.t()) :: non_neg_integer()
-  def score(<<score::16, _::binary>>), do: score
+  def score(<<_::1, score::15, _::binary>>), do: score
 
-  @spec roll(Game.t(), non_neg_integer()) :: Game.t()
-  def roll(game, amount) do
-    <<_::16, roll::2, frame::2, _::binary>> = game
+  @spec roll(Game.t(), non_neg_integer()) :: Game.t() | {:error, error_msg}
+  def roll(<<1::1, _::binary>>), do: {:error, :game_over}
+
+  def roll(game, amount) when is_integer(amount) do
+    game
+    |> add_roll(amount)
+    |> calculate_score
+  end
+
+  @spec details(Game.t()) :: String.t()
+  def details(<<over::1, score::15, roll::2, frame::6, frames::bytes-size(11), name::binary>>) do
+    frame_formatter = fn
+      {r1, r2} -> "#{r1} #{r2} | "
+      {r1, r2, r3} -> "#{r1} #{r2} #{r3}"
+    end
+
+    """
+    Player: #{name}
+    Status: #{if over == 0, do: "Playing", else: "Game Over"}
+    Ball: #{roll + 1}
+    Frame: #{frame + 1}
+    Score: #{score}
+    Frames: #{each_frame(frames, frame_formatter)}
+    """
+  end
+
+  defp add_roll(<<s::16, roll::2, frame::6, frames::bytes-size(11), n::binary>>, amount) do
+    <<prior::bytes-size(frame), roll1::4, roll2::4, rem::binary>> = frames
 
     case {frame, roll, amount} do
-      _ -> "TODO"
+      {9, 0, 10} ->
+        <<s::16, 1::2, frame::6, prior::binary, 10::4, 0::4, rem::binary, n::binary>>
+
+      {9, 1, 10} ->
+        <<s::16, 2::2, frame::6, prior::binary, roll1::4, 10::4, rem::binary, n::binary>>
+
+      {9, 1, _} ->
+        if amount + roll1 == 10 do
+          <<s::16, 2::2, frame::6, prior::binary, roll1::4, amount::4, rem::binary, n::binary>>
+        else
+          game_over(
+            <<s::16, 1::2, frame::6, prior::binary, roll1::4, amount::4, rem::binary, n::binary>>
+          )
+        end
+
+      {9, 2, _} ->
+        game_over(
+          <<s::16, 2::2, frame::6, prior::binary, roll1::4, roll2::4, amount::4, 0::4, n::binary>>
+        )
+
+      {_, 0, 10} ->
+        <<s::16, 0::2, frame + 1::6, prior::binary, 10::4, 0::4, rem::binary, n::binary>>
+
+      {_, 0, _} ->
+        <<s::16, 1::2, frame::6, prior::binary, amount::4, 0::4, rem::binary, n::binary>>
+
+      {_, 1, _} ->
+        <<s::16, 0::2, frame + 1::6, prior::binary, roll1::4, amount::4, rem::binary, n::binary>>
     end
+  end
+
+  defp calculate_score(<<over::1, _::15, postion::8, frames::bytes-size(11), n::binary>>) do
+    score =
+      frame_list(frames)
+      |> Enum.chunk_every(3, 1, [])
+      |> Enum.reduce(0, fn
+        [{10, 0}, {10, 0}, {a, _}], sum ->
+          sum + 20 + a
+
+        [{10, 0}, {a, b}, {_, _}], sum ->
+          sum + 10 + a + b
+
+        [{a, b}, {c, _}, _], sum when a + b == 10 ->
+          sum + 10 + c
+
+        [{a, b}, _, _], sum ->
+          sum + a + b
+
+        _unknown, sum ->
+          # IO.inspect(unknown)
+          sum
+      end)
+
+    <<over::1, score::15, postion::8, frames::binary, n::binary>>
+  end
+
+  defp game_over(<<_::1, score::15, rem::binary>>), do: <<1::1, score::15, rem::binary>>
+
+  defp frame_list(frames) do
+    each_frame(frames, & &1)
+  end
+
+  defp each_frame(frames, fun) do
+    do_each_frame(frames, fun, [])
+  end
+
+  defp do_each_frame(<<roll1::4, roll2::4, roll3::4, _::4>>, fun, acc) do
+    Enum.reverse([fun.({roll1, roll2, roll3}) | acc])
+  end
+
+  defp do_each_frame(<<roll1::4, roll2::4, remaining::binary>>, fun, acc) do
+    do_each_frame(remaining, fun, [fun.({roll1, roll2}) | acc])
   end
 end
--- test/binary_bowling/game_test.exs
+++ test/binary_bowling/game_test.exs
@@ -12,6 +12,24 @@ defmodule BinaryBowling.GameTest do
   end
 
   test "score/1" do
-    assert 0 == Game.new |> Game.score
+    assert 0 == Game.new() |> Game.score()
+  end
+
+  describe "roll/2" do
+    test "twenty gutter balls" do
+      game =
+        Enum.reduce(1..20, Game.new("ben"), fn _, game ->
+          Game.roll(game, 0)
+        end)
+
+      # Game is over with no score, frame has been advanced
+      # to the last possible roll, and all frame data is empty
+      assert <<1::1, 0::15, 1::2, 9::6, 0::88, "ben">> = game
+    end
+
+    test "one strike" do
+      game = "ben" |> Game.new() |> Game.roll(10)
+      assert <<0::1, 10::15, 0::2, 1::6, 10::4, 0::84, "ben">> = game
+    end
   end
 end
